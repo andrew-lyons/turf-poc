b0ab8e3ae12e3b3f5f17bfb20ca094111838e76c
Andrew Lyons <aslyons001@gmail.com>
2021-01-29 18:36:45 -0500

    added real data, working on proper map highlights

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -13,38 +13,29 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYXNseW9uczAwMSIsImEiOiJja2toZGhxN24wYTFrMm5xa
             }
         };
 
+        fetch('https://andrew-lyons.github.io/turf-poc/public/json/mcd_hic_fc_p1.json')
+            .then((res) => res.json())
+            .then(data => formatToGeo(data))
 
 var mcData =  {
     type: 'FeatureCollection',
-    features: [
-        { 
-        // I prettified this geoJSON for readability, it's nothing different from the below objects
-            type: 'Feature', 
-            properties: { 
-                Name: "McDonald's #1",
-                Address: 'Lorem Ipsum Road' 
-            }, 
-            geometry: {
-                type: 'Point',
-                coordinates: [-81.449181, 39.420829]
-            }
-        },
-        { type: 'Feature', properties: { Name: "McDonald's #2", Address: 'Lorem Ipsum Road' }, geometry: { type: 'Point', coordinates: [-81.547325, 39.390173] } },
-        { type: 'Feature', properties: { Name: "McDonald's #3", Address: 'Lorem Ipsum Road' }, geometry: { type: 'Point', coordinates: [-81.530148, 39.269607] } },
-        { type: 'Feature', properties: { Name: "McDonald's #4", Address: 'Lorem Ipsum Road' }, geometry: { type: 'Point', coordinates: [-81.196410, 39.396408] } },
-        { type: 'Feature', properties: { Name: "McDonald's #5", Address: 'Lorem Ipsum Road' }, geometry: { type: 'Point', coordinates: [-82.803338, 39.872858] } },
-    ]
+    features: []
 };
 
 var myLocation = {
-    type: 'FeatureCollection',
-    features: [
-        { type: 'Feature', properties: { Name: 'Me! #1', Address: null }, geometry: { type: 'Point', coordinates: [-81.433140, 39.327393] } },
-        { type: 'Feature', properties: { Name: 'Me! #2', Address: null }, geometry: { type: 'Point', coordinates: [-82.686895, 39.851353] } }
-    ]
-};
+        type: 'Feature',
+        properties: {
+            Name: 'Me! #1',
+            Address: null
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [-81.433140, 39.327393]
+        }
+    };
 
 map.on('load', function() {
+
     map.addLayer({
         id: 'mcData',
         type: 'symbol',
@@ -75,8 +66,7 @@ map.on('load', function() {
         type: 'geojson',
         data: {
             type: 'FeatureCollection',
-            features: [
-            ]
+            features: []
         }
     });
 });
@@ -102,60 +92,50 @@ map.on('mousemove', function(e) {
 
 map.on('click', function(e) {
 
-    var myLocationFeatures = map.queryRenderedFeatures(e.point, { layers: ['myLocation'] });
-    if (!myLocationFeatures.length) {
-        return;
-    }
-  
-    var myLocationFeature = myLocationFeatures[0];
-  
-    var nearestMcData = turf.nearest(myLocationFeature, mcData);
-        
-    top3()
-    
-    if (nearestMcData !== null) {
-  
-        map.getSource('nearest-food').setData({
-            type: 'FeatureCollection',
-            features: [nearestMcData]
-        });
-  
-        map.addLayer({
-            id: 'nearest-food',
-            type: 'circle',
-            source: 'nearest-food',
-            paint: {
-                'circle-radius': 12,
-                'circle-color': '#486DE0'
-            }
-        }, 'mcData');
-    }
+    var i = 0;
+    var closestPoints = [];
+
+    while(i < 13) {
+        var geoJ = turf.nearest(myLocation, mcData)
+        closestPoints.push(geoJ);
+        var id = geoJ.properties.featureIndex;
+        //remove from features point that was found
+        mcData.features.splice(id, 1);
+        i++;
+    };
+
+    console.log(closestPoints)
+    console.log(typeof(closestPoints))
+    map.getSource('nearest-food').setData({
+        type: 'FeatureCollection',
+        features: closestPoints
+    });
+
+    map.addLayer({
+        id: 'nearest-food',
+        type: 'circle',
+        source: 'nearest-food',
+        paint: {
+            'circle-radius': 12,
+            'circle-color': '#486DE0'
+        }
+    }, 'mcData');
 });
 
-
-
-top3 = () => {
-
-    // instantiate this for readability
-    var me = myLocation.features[0].geometry.coordinates
-
-    // Map through mcData (large geoJSON array) and return coordinate
-    // distance function from turf. Then sort to get in ascending order
-    // (smallest to largest) 
-    var nearestArray = mcData.features.map((i) => {
-
-        return turf.distance(i, me, 'miles').toFixed(2)
-
-    }).sort(function(a, b) { return a - b; });
-
-    // Hard coded names :(, distances are correct, as they come from properly sorted array
-    console.log(
-        '\n',
-        "Your closest 3 points are: ", '\n \n'
-        + mcData.features[0].properties.Name + " at " + mcData.features[0].properties.Address + ", " +  nearestArray[0] + " miles away, ", '\n'
-        + mcData.features[2].properties.Name + " at " + mcData.features[2].properties.Address + ", " +  nearestArray[1] + " miles away, and ", '\n'
-        + mcData.features[1].properties.Name + " at " + mcData.features[1].properties.Address + ", " +  nearestArray[2] + " miles away!", '\n \n'
-    )
-
-    return nearestArray
+formatToGeo = (data) => {
+    for (i=0; i < data.length; i++) {
+        mcData.features.push(
+            {
+                type: 'Feature',
+                properties: {
+                    Name: data[i].name,
+                    Address: data[i].address
+                },
+                geometry: {
+                    type: 'Point',
+                    coordinates: [data[i].lonlat[0], data[i].lonlat[1]]
+                }
+            }
+        )
+    }
 }
